b9d6d0ed16838bcddfbf09c9f5d214fd753a50d8
Ricardo Marmolejo García <makiolo@gmail.com>
2015-04-26 01:51:57 +0200

    fixed scheduler

--- tests/test_leader_follower.cpp
+++ tests/test_leader_follower.cpp
@@ -14,103 +14,112 @@
 #include <assert.h>
 #include <atomic>
 #include <exception>
-#include <condition_variable>
+#include <mutex>
 
 namespace lead {
 
 template <typename T> using CommandTalker = std::function<void(T&)>;
 template <typename T> using CompositeCommandTalker = std::function<CommandTalker<T>(const CommandTalker<T>&)>;
 
-template <typename T>
-CommandTalker<T> operator>>=(const CompositeCommandTalker<T>& a, const CommandTalker<T>& b)
-{
-	return a(b);
-}
-
-template <typename T>
-CompositeCommandTalker<T> repeat(int n)
-{
-	return [=](const CommandTalker<T>& f)
-	{
-		return [&](T& self)
-		{
-			// I want coroutines :(
-			for(int cont = 0; cont < n; ++cont)
-			{
-				f(self);
-			}
-		};
-	};
-}
-
-template <typename T>
-class talker
+template <typename SELF, typename T>
+class scheduler
 {
 public:
-	using container = fes::queue_delayer<CommandTalker<T> >;
+	using command_other = CommandTalker<T> ;
+	using command_reflexion = CommandTalker<SELF> ;
+	using container_other = fes::queue_delayer<command_other>;
+	using container_reflexion = fes::queue_delayer<command_reflexion>;
 	
-	talker(const std::string& name)
+	scheduler(const std::string& name)
 		: _name(name)
-		, _thread(nullptr)
-		, _finish(false)
+		, _busy_other(false)
+		, _busy_me(false)
+	{
+		_conn_me = _commands_me.connect(std::bind(&scheduler::planificator_me, this, std::placeholders::_1));
+	}
+	~scheduler()
 	{
 		
 	}
-	~talker()
-	{
-		
-	}
+
+	scheduler(const scheduler&) = delete;
+	scheduler& operator=(const scheduler&) = delete;
 	
 	void add_follower(T& talker)
 	{
-		_conns.emplace_back(_queue.connect(std::bind(&talker::planificator, *this, std::ref(talker), std::placeholders::_1)));
+		_conns.emplace_back(_commands_other.connect(std::bind(&scheduler::planificator_other, this, std::ref(talker), std::placeholders::_1)));
 	}
 	
-	void planificator(T& talker, const CommandTalker<T>& cmd)
+	void planificator_me(const command_reflexion& cmd)
 	{
-		_thread = std::make_shared<std::thread>([&](T& obj){
-			cmd(obj);
-			_finish = true;
-		}, std::ref(talker));
+		std::thread th([&]()
+		{
+			cmd(static_cast<SELF&>(*this));
+			_busy_me = false;
+		});
+		th.detach();
 	}
-	
-	inline void order(const CommandTalker<T>& command, int milli = 0, int priority = 0)
+
+	void planificator_other(T& talker, const command_other& cmd)
 	{
-		_queue(priority, std::chrono::milliseconds(milli), command);
+		std::thread th([&]()
+		{
+			cmd(talker);
+			_busy_other = false;
+		});
+		th.detach();
+	}
+
+	inline void call_async(const command_reflexion& command, int milli = 0, int priority = 0)
+	{
+		_commands_me(priority, std::chrono::milliseconds(milli), command);
+	}
+
+	inline void call_followers(const command_other& command, int milli = 0, int priority = 0)
+	{
+		_commands_other(priority, std::chrono::milliseconds(milli), command);
 	}
 	
 	void update()
 	{
-		if (_thread == nullptr)
+		if (!_busy_other)
 		{
-			_queue.dispatch();
+			_busy_other = _commands_other.dispatch();
 		}
-		else
+		if (!_busy_me)
 		{
-			if (_finish)
-			{
-				_thread->join();
-				_thread = nullptr;
-				_finish = false;
-			}
+			_busy_me = _commands_me.dispatch();
 		}
 	}
 
-	inline bool empty() const
-	{
-		return _queue.empty();
-	}
-	
-	inline void wait(int milli)
+	inline void sleep(int milli)
 	{
 		std::this_thread::sleep_for( std::chrono::milliseconds(milli) );
 	}
+
+	void signal()
+	{
+		_signal.notify_one();
+	}
+	
+	void wait()
+	{
+		std::unique_lock<std::mutex> context(_signal_lock);
+		_signal.wait(context);
+	}
 protected:
-	std::vector<fes::shared_connection<CommandTalker<T> > > _conns;
-	container _queue;
 	std::string _name;
-	std::shared_ptr<std::thread> _thread;
-	bool _finish;
+	// me self
+	fes::shared_connection<command_reflexion> _conn_me;
+	container_reflexion _commands_me;
+	std::atomic<bool> _busy_me;
+	// others
+	std::vector<fes::shared_connection<command_other> > _conns;
+	container_other _commands_other;
+	std::atomic<bool> _busy_other;
+	// 
+	std::condition_variable _signal;
+	std::mutex _signal_lock;
 };
 
 }
@@ -125,18 +134,22 @@ public:
 	
 	~Context() { ; }
 
-	void print(const std::string& text)
+	inline void sleep(int milli)
 	{
-		_lock.lock();
+		std::this_thread::sleep_for( std::chrono::milliseconds(milli) );
+	}
 
+	void print(const std::string& name, const std::string& text, int delay = 10)
+	{
+		std::unique_lock<std::mutex> context(_lock);
+
+		std::cout << "<" << name << "> ";
 		for(const char& c : text)
 		{
 			std::cout << c << std::flush;
-			//std::this_thread::sleep_for( std::chrono::milliseconds(100) );
+			sleep(delay);
 		}
 		std::cout << std::endl;
-
-		_lock.unlock();
 	}
 protected:
 	std::mutex _lock;
@@ -144,11 +157,11 @@ protected:
 
 class Buyer;
 
-class ShopKeeper : public lead::talker<Buyer>
+class ShopKeeper : public lead::scheduler<ShopKeeper, Buyer>
 {
 public:
 	explicit ShopKeeper(const std::string& name, Context& context)
-		: talker(name)
+		: scheduler(name)
 		, _context(context)
 	{
 		
@@ -156,82 +169,117 @@ public:
 	
 	~ShopKeeper() { ; }
 	
-	void say(const std::string& text)
+	void say(const std::string& text, int delay = 10)
 	{
-		_context.print(text);
+		_context.print(_name, text, delay);
 	}
 protected:
 	Context& _context;
 };
 
 
-class Buyer : public lead::talker< ShopKeeper >
+class Buyer : public lead::scheduler<Buyer, ShopKeeper>
 {
 public:
 	explicit Buyer(const std::string& name, Context& context)
-		: talker(name)
+		: scheduler(name)
 		, _context(context)
 	{
 		
 	}
 	~Buyer() { ; }	
-	Buyer(const Buyer&) = delete;
 
-	// skills
-	void say(const std::string& text)
+	void say(const std::string& text, int delay = 10)
 	{
-		_context.print(text);
+		_context.print(_name, text, delay);
 	}
 protected:
 	Context& _context;
 };
 
+/*
+lead::CommandTalker<Buyer> operator>>=(const lead::CompositeCommandTalker<Buyer>& a, const lead::CommandTalker<Buyer>& b)
+{
+	return a(b);
+}
+
+lead::CompositeCommandTalker<Buyer> repeat(int n)
+{
+	return [=](const lead::CommandTalker<Buyer>& f)
+	{
+		return [&](Buyer& self)
+		{
+			for(int cont = 0; cont < n; ++cont)
+			{
+				f(self);
+			}
+		};
+	};
+}
+*/
+
 int main()
 {
+	std::ios_base::sync_with_stdio(false);
+
 	{
 		Context context;
-		Buyer buyer("buyer", context);
+		Buyer buyer("tu", context);
 		ShopKeeper shopkeeper("shopkeeper", context);
-		
-		buyer.add_follower(shopkeeper);
 		shopkeeper.add_follower(buyer);
 		
-		// conversation between tyrants
+		buyer.call_async([&](Buyer& self) {
+			self.say("Te despiertas en la carcel. No te acuerdas de nada.");
+			self.sleep(100);
+			self.say("	1. Examinar la celda");
+			self.sleep(100);
+			self.say("	2. Seguir durmiendo");
+			self.sleep(100);
+
+			int choose = -1;
+			while (choose == -1)
+			{
+				if (!(std::cin >> choose))
+				{
+					std::cout << "Option not is a number" << std::endl;
+				}
+				else if (choose == 1)
+				{
+					self.call_async([&](Buyer& self) {
+						self.say("Has elegido: Examinar la habitacion");
+						self.sleep(100);
+						self.say("Solo hay una manta sucia y un orinal");
+						self.sleep(100);
+					});
+				}
+				else if (choose == 2)
+				{
+					self.call_async([&](Buyer& self) {
+						self.say("Has elegido: seguir durmiendo!");
+						self.sleep(100);
+						self.say("ZzZzZz ...", 500);
+						self.sleep(200);
+					});
+				}
+				else
+				{
+					std::cout << "Invalid option" << std::endl;
+				}
+			}
+
+		});
 		
-		buyer.order([&](ShopKeeper& self) {
-				self.say("Hi!");
-				self.wait(10);
-				self.say("Hiiiiiiiiiiii!");
-		}, 1);
-
-		shopkeeper.order([&](Buyer& self) {
-				self.say("How much cost are the apples?");
-				self.wait(10);
-				self.say("How much cost are the apples???????????");
-		}, 2);
-
-		buyer.order([&](ShopKeeper& self) {
-				self.say("50 cents each apple"); 
-		}, 3);
-
-		shopkeeper.order([&](Buyer& self) {
-				self.say("I want apples!");
-		}, 4);
-
-		buyer.order([&](ShopKeeper& self) {
-				self.say("You apples, thanks"); 
-		}, 5);
-
-		shopkeeper.order([&](Buyer& self) {
-				return self.say("Bye!");
-		}, 6);
-	
-		for(int i=0; i<9000;++i)	
+		
+#ifdef _WIN32
+		while (true)
+#else
+		for (int i = 0; i < 9000; ++i)
+#endif
 		{
 			buyer.update();
 			shopkeeper.update();
-
-			std::this_thread::sleep_for( std::chrono::milliseconds(1) );
+			//
+			shopkeeper.sleep(1);
 		}
 	}
 	return(0);
