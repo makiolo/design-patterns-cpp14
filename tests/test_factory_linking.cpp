97a80dddc5461ad2b4b6e7c0568b50ff490a5e6c
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2016-01-16 21:00:55 +0100

    revert unregister

--- tests/test_factory_linking.cpp
+++ tests/test_factory_linking.cpp
@@ -62,20 +62,23 @@ int main()
 
 	load_library fooA("fooA");
 	load_library fooB("fooB");
+	{
+		load_library foo("foo");
 
-	// equivalent ways of create A
-	std::shared_ptr<Base> a1 = Base::get_factory().create<A>("first parameter", 2);
-	std::shared_ptr<A> a2 = Base::get_factory().create<A>("first parameter", 2);
-	std::shared_ptr<Base> a3 = Base::get_factory().create(A::KEY(), "first parameter", 2);
+		// equivalent ways of create A
+		std::shared_ptr<Base> a1 = Base::get_factory().create<A>("first parameter", 2);
+		std::shared_ptr<A> a2 = Base::get_factory().create<A>("first parameter", 2);
+		std::shared_ptr<Base> a3 = Base::get_factory().create(A::KEY(), "first parameter", 2);
 
-	// equivalent ways of create B
-	std::shared_ptr<Base> b1 = Base::get_factory().create<B>("first parameter", 2);
-	std::shared_ptr<B> b2 = Base::get_factory().create<B>("first parameter", 2);
-	std::shared_ptr<Base> b3 = Base::get_factory().create(B::KEY(), "first parameter", 2);
+		// equivalent ways of create B
+		std::shared_ptr<Base> b1 = Base::get_factory().create<B>("first parameter", 2);
+		std::shared_ptr<B> b2 = Base::get_factory().create<B>("first parameter", 2);
+		std::shared_ptr<Base> b3 = Base::get_factory().create(B::KEY(), "first parameter", 2);
 
-	assert(a1 != a2);
-	assert(a3 != b1);
-	assert(b1 != b2);
+		assert(a1 != a2);
+		assert(a3 != b1);
+		assert(b1 != b2);
+	}
 
 	return(0);
 }
