232d7aa36673844d33ef4ed5ad20999b6af88293
Ricardo <makiolo@gmail.com>
2017-08-28 15:05:18 +0200

    Update test_repository_once_static.cpp

--- tests/test_repository_once_static.cpp
+++ tests/test_repository_once_static.cpp
@@ -9,31 +9,46 @@ using repo = dp14::repository_once<float, std::string>;
 
 struct foo : dp14::code_once<float, std::string>
 {
-    float execute(const std::string& payload) const override
-    {
-	std::cout << "foo: payload = " << payload << std::endl;
-	return 1.0f;
-    }
+	foo(const std::string& payload)
+	{
+		;
+	}
+
+	float execute(const std::string& payload) const override
+	{
+		std::cout << "foo: payload = " << payload << std::endl;
+		return 1.0f;
+	}
 };
 DEFINE_HASH_CUSTOM(foo, float, 1.0f)
 
 struct reb : dp14::code_once<float, std::string>
-{   
-    float execute(const std::string& payload) const override
-    {
-    	std::cout << "reb: payload = " << payload << std::endl;
-	return 2.0f;
-    }
+{
+	reb(const std::string& payload)
+	{
+		;
+	}
+
+	float execute(const std::string& payload) const override
+	{
+		std::cout << "reb: payload = " << payload << std::endl;
+		return 2.0f;
+	}
 };
 DEFINE_HASH_CUSTOM(reb, float, 2.0f)
 
 struct tol : dp14::code_once<float, std::string>
 {
-    float execute(const std::string& payload) const override
-    {
-	std::cout << "tol: payload = " << payload << std::endl;
-	return 3.0f;
-    }
+	tol(const std::string& payload)
+	{
+		;
+	}
+
+	float execute(const std::string& payload) const override
+	{
+		std::cout << "tol: payload = " << payload << std::endl;
+		return 3.0f;
+	}
 };
 DEFINE_HASH_CUSTOM(tol, std::string, "/homie/salon/temperature")
 
