e5030ea2ee569aa721dcb765be1095ecb1834108
Ricardo <makiolo@gmail.com>
2017-08-28 09:59:13 +0200

    Update test_repository_static.cpp

--- tests/test_repository_static.cpp
+++ tests/test_repository_static.cpp
@@ -4,11 +4,10 @@
 #include <functional>
 #include <metacommon/common.h>
 #include "../factory.h"
-#include "../memoize.h"
 
-using repo = dp14::repository_once<std::string, std::string>;
+using repo = dp14::repository<std::string, std::string>;
 
-struct foo : dp14::code_once<std::string, std::string>
+struct foo : dp14::code<std::string, std::string>
 {
     foo(const std::string& topic, const std::string& payload)
     {
@@ -17,7 +16,7 @@ struct foo : dp14::code_once<std::string, std::string>
 };
 DEFINE_HASH_CUSTOM(foo, float, 1.0f)
 
-struct reb : dp14::code_once<std::string, std::string>
+struct reb : dp14::code<std::string, std::string>
 {
     reb(const std::string& topic, const std::string& payload)
     {
@@ -26,7 +25,7 @@ struct reb : dp14::code_once<std::string, std::string>
 };
 DEFINE_HASH_CUSTOM(reb, float, 2.0f)
 
-struct tol : dp14::code_once<std::string, std::string>
+struct tol : dp14::code<std::string, std::string>
 {
     tol(const std::string& topic, const std::string& payload)
     {
@@ -37,15 +36,15 @@ DEFINE_HASH_CUSTOM(tol, std::string, "/homie/salon/temperature")
 
 int main()
 {
-    repo r;
-    repo::reg<foo> r1(r);
-    repo::reg<reb> r2(r);
-    repo::reg<tol> r3(r);
-    r.get("/homie/salon/temperature", "tol", "24.0");
-	r.get("/homie/salon/temperature", "tol", "24.0");
-	r.get("/homie/salon/temperature", "tol", "24.0");
-	r.get("/homie/salon/temperature", "tol", "25.0");
-	r.get("/homie/salon/temperature", "tol", "25.0");
-	r.get("/homie/salon/temperature", "tol", "25.0");
-    return 0;
+	repo r;
+	repo::reg<foo> r1(r);
+	repo::reg<reb> r2(r);
+	repo::reg<tol> r3(r);
+	r.create("/homie/salon/temperature", "tol", "24.0");
+	r.create("/homie/salon/temperature", "tol", "24.0");
+	r.create("/homie/salon/temperature", "tol", "24.0");
+	r.create("/homie/salon/temperature", "tol", "25.0");
+	r.create("/homie/salon/temperature", "tol", "25.0");
+	r.create("/homie/salon/temperature", "tol", "25.0");
+	return 0;
 }
