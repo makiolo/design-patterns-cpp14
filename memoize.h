32e8468f409cdc0bed05839a7d1dc4e08a0043d7
Ricardo <makiolo@gmail.com>
2017-08-28 12:50:12 +0200

    Update memoize.h

--- memoize.h
+++ memoize.h
@@ -251,7 +251,7 @@ struct code_once
 	virtual ~code_once() { ; }
 	void set(Result r) {_r = std::move(r);}
 	Result get() const {return _r;}
-	virtual Result execute(Args&&... args) = 0;
+	virtual Result execute(Args&&... args) const = 0;
 protected:
 	Result _r;
 };
