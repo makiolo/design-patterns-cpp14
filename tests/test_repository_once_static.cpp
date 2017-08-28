f8d7f621c1247ebe4d1ca9ca96a20cb02640a3e5
Ricardo <makiolo@gmail.com>
2017-08-28 11:23:28 +0200

    Update test_repository_once_static.cpp

--- tests/test_repository_once_static.cpp
+++ tests/test_repository_once_static.cpp
@@ -5,31 +5,31 @@
 #include <metacommon/common.h>
 #include "../memoize.h"
 
-using repo = dp14::repository_once<std::string, std::string>;
+using repo = dp14::repository_once<std::string>;
 
-struct foo : dp14::code_once<std::string, std::string>
+struct foo : dp14::code_once<std::string>
 {
-    foo(const std::string& topic, const std::string& payload)
+    foo(const std::string& payload)
     {
-		std::cout << "foo: topic: " << topic << ", payload = " << payload << std::endl;
+		std::cout << "foo: payload = " << payload << std::endl;
     }
 };
 DEFINE_HASH_CUSTOM(foo, float, 1.0f)
 
-struct reb : dp14::code_once<std::string, std::string>
+struct reb : dp14::code_once<std::string>
 {
-    reb(const std::string& topic, const std::string& payload)
+    reb(const std::string& payload)
     {
-		std::cout << "reb: topic: " << topic << ", payload = " << payload << std::endl;
+		std::cout << "reb: payload = " << payload << std::endl;
     }
 };
 DEFINE_HASH_CUSTOM(reb, float, 2.0f)
 
-struct tol : dp14::code_once<std::string, std::string>
+struct tol : dp14::code_once<std::string>
 {
-    tol(const std::string& topic, const std::string& payload)
+    tol(const std::string& payload)
     {
-		std::cout << "tol: topic: " << topic << ", payload = " << payload << std::endl;
+		std::cout << "tol: payload = " << payload << std::endl;
     }
 };
 DEFINE_HASH_CUSTOM(tol, std::string, "/homie/salon/temperature")
@@ -40,19 +40,23 @@ int main()
 	repo::reg<foo> r1(r);
 	repo::reg<reb> r2(r);
 	repo::reg<tol> r3(r);
-	r.execute("/homie/salon/temperature", "tol", "24.0");
-	r.execute("/homie/salon/temperature", "tol", "24.0");
-	r.execute("/homie/salon/temperature", "tol", "24.0");
-	r.execute("/homie/salon/temperature", "tol", "25.0");
-	r.execute("/homie/salon/temperature", "tol", "25.0");
-	r.execute("/homie/salon/temperature", "tol", "25.0");
+	r.execute("/homie/salon/temperature", "24.0");
+	r.execute("/homie/salon/temperature", "24.0");
+	r.execute("/homie/salon/temperature", "24.0");
+	r.execute("/homie/salon/temperature", "25.0");
+	r.execute("/homie/salon/temperature", "25.0");
+	r.execute("/homie/salon/temperature", "25.0");
 	std::cout << "clearing cache" << std::endl;
 	r.clear();
-	r.execute("/homie/salon/temperature", "tol", "24.0");
-	r.execute("/homie/salon/temperature", "tol", "24.0");
-	r.execute("/homie/salon/temperature", "tol", "24.0");
-	r.execute("/homie/salon/temperature", "tol", "25.0");
-	r.execute("/homie/salon/temperature", "tol", "25.0");
-	r.execute("/homie/salon/temperature", "tol", "25.0");
+	r.execute("/homie/salon/temperature", "24.0");
+	r.execute("/homie/salon/temperature", "24.0");
+	r.execute("/homie/salon/temperature", "24.0");
+	r.execute("/homie/salon/temperature", "25.0");
+	r.execute("/homie/salon/temperature", "25.0");
+	r.execute("/homie/salon/temperature", "25.0");
+	r.execute(1.0f, "register with 1.0f");
+	r.execute(1.0f, "register with 1.0f");
+	r.execute(2.0f, "register with 2.0f");
+	r.execute(2.0f, "register with 2.0f");
 	return 0;
 }
