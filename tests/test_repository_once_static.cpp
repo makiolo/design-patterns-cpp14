93c2e568a5a9f6c95d3cca0355e6f2578e86afbd
Ricardo <makiolo@gmail.com>
2017-08-30 12:28:48 +0200

    Update test_repository_once_static.cpp

--- tests/test_repository_once_static.cpp
+++ tests/test_repository_once_static.cpp
@@ -3,8 +3,9 @@
 #include <map>
 #include <functional>
 #include <metacommon/common.h>
-#include "../memoize.h"
 #include <gmock/gmock.h>
+#include "../memoize.h"
+
 class RepositoryOnceStaticTests : testing::Test {};
 
 using repo = dp14::repository_once<float, std::string>;
@@ -45,43 +46,48 @@ TEST(RepositoryOnceStaticTests, Test1)
 	repo::reg<foo> r1(r);
 	repo::reg<reb> r2(r);
 	repo::reg<tol> r3(r);
+	// calcula
 	std::cout << r.execute("/homie/salon/temperature", "24.0") << std::endl;
+	// cachea
 	std::cout << r.execute("/homie/salon/temperature", "24.0") << std::endl;
+	// cachea
 	std::cout << r.execute("/homie/salon/temperature", "24.0") << std::endl;
+	// calcula
 	std::cout << r.execute("/homie/salon/temperature", "25.0") << std::endl;
+	// cachea
 	std::cout << r.execute("/homie/salon/temperature", "25.0") << std::endl;
+	// cachea
 	std::cout << r.execute("/homie/salon/temperature", "25.0") << std::endl;
+	// limpia cache
 	std::cout << "clearing cache" << std::endl;
 	r.clear();
+	// calcula
 	std::cout << r.execute("/homie/salon/temperature", "24.0") << std::endl;
+	// cachea
 	std::cout << r.execute("/homie/salon/temperature", "24.0") << std::endl;
+	// cachea
 	std::cout << r.execute("/homie/salon/temperature", "24.0") << std::endl;
+	// calcula
 	std::cout << r.execute("/homie/salon/temperature", "25.0") << std::endl;
+	// cachea
 	std::cout << r.execute("/homie/salon/temperature", "25.0") << std::endl;
+	// cachea
 	std::cout << r.execute("/homie/salon/temperature", "25.0") << std::endl;
+	// calcula
 	std::cout << r.execute(1.0f, "register with 1.0f") << std::endl;
+	// cachea
 	std::cout << r.execute(1.0f, "register with 1.0f") << std::endl;
+	// calcula
 	std::cout << r.execute(2.0f, "register with 2.0f") << std::endl;
+	// cachea
 	std::cout << r.execute(2.0f, "register with 2.0f") << std::endl;
+	// limpia cache
+	std::cout << "clearing cache" << std::endl;
 	r.clear();
+	// calcula
 	std::cout << r.execute(2.0f, "register with 2.0f") << std::endl;
+	// limpiar cache de un elmento especifico
 	r.clear(2.0f, "register with 2.0f");
+	// calcula
 	std::cout << r.execute(2.0f, "register with 2.0f") << std::endl;
-}
-
-TEST(RepositoryOnceStaticTests, TestMemoryLeak)
-{
-	// example memory leak
-	int* pn = new int(123);
-	std::cout << "leak test number: " << *pn << std::endl;
-	ASSERT_EQ(*pn, 123);
-	pn = new int(567);
-	std::cout << "leak test number: " << *pn << std::endl;
-	ASSERT_EQ(*pn, 567);
-	
-}
-
-TEST(RepositoryOnceStaticTests, TestMemoryLeak2)
-{
-	char *x = malloc(100); /* or, in C++, "char *x = new char[100] */
 }
