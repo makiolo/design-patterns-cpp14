49e65590d7e2fb2a38ee7e5798a19ac49a495a38
Ricardo <makiolo@gmail.com>
2019-04-17 16:04:58 +0200

    .

--- tests/test_memoize_static.cpp
+++ tests/test_memoize_static.cpp
@@ -3,10 +3,10 @@
 #include <assert.h>
 #include <memoize.h>
 #include <gmock/gmock.h>
-#include <json.hpp>
+// #include <json.hpp>
 #include <fstream>
 
-using json = nlohmann::json;
+// using json = nlohmann::json;
 
 class MemoizeStaticTests : testing::Test {};
 
@@ -81,57 +81,22 @@ bool exists(const char *fileName)
 	return infile.good();
 }
 
-TEST(MemoizeStaticTests, DISABLED_TestJSON)
-{
-	json j3;
-	for(int i = 0; i < 2; ++i)
-	{
-		// default json
-		j3 = R"(
-		  {
-			"happy": true,
-			"happy2": false,
-			"pi": 3.141
-		  }
-		)"_json;
-		
-		std::ofstream out("example.json");
-		out << std::setw(4) << j3 << std::endl;
-
-		/*
-		if(exists("example.bin"))
-		{
-			std::basic_ifstream<std::uint32_t> file_bin("example.bin");
-			file_bin.seekg(0, std::ios::end);
-			size_t len = file_bin.tellg();
-			std::uint32_t* buffer = new std::uint32_t[len];
-			file_bin.seekg(0, std::ios::beg);
-			file_bin.read(buffer, len);
-			file_bin.close();
-			std::vector<std::uint8_t> v_cbor(len);
-			for(size_t j=0;j<len; ++j)
-				v_cbor.push_back(buffer[j]);
-			std::cout << "read " << v_cbor.size() << " bytes (uint8)" << std::endl;
-			// j3 = json::from_cbor(v_cbor);
-			j3 = json::from_msgpack(v_cbor);
-			delete [] buffer;
-		}
-		else
-		{
-			std::ifstream file_json("example.json");
-			file_json >> j3;
-		}
-		std::cout << j3.dump(4) << std::endl;
-
-		std::ofstream o("example.bin");
-		std::vector<std::uint8_t> v_cbor = json::to_msgpack(j3);
-		size_t len = v_cbor.size();
-		std::cout << "write " << v_cbor.size() << " bytes (uint8)" << std::endl;
-		std::uint32_t* buffer = new std::uint32_t[len];
-		std::copy(v_cbor.begin(), v_cbor.end(), buffer);
-		o << buffer;
-		delete [] buffer;
-		*/
-	}
-}
+// TEST(MemoizeStaticTests, DISABLED_TestJSON)
+// {
+// 	json j3;
+// 	for(int i = 0; i < 2; ++i)
+// 	{
+// 		// default json
+// 		j3 = R"(
+// 		  {
+// 			"happy": true,
+// 			"happy2": false,
+// 			"pi": 3.141
+// 		  }
+// 		)"_json;
+// 		
+// 		std::ofstream out("example.json");
+// 		out << std::setw(4) << j3 << std::endl;
+// 	}
+// }
 
