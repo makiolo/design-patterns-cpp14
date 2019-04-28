2183fae9c1d8c384b73c1748a45207cb4a3f7f7f
Ricardo Marmolejo García <makiolo@gmail.com>
2019-04-28 16:14:35 +0200

    .

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
 ARG IMAGE
 FROM $IMAGE
 
+RUN curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+RUN sudo apt-get install -y nodejs
+
 ARG MODE
 ARG COMPILER
 ARG COMPILER_VERSION
 ARG COMPILER_LIBCXX
 ARG CC
 ARG CXX
-ARG PACKAGE
 ARG CONAN_TOKEN
+ARG NPP_CI=FALSE
 
 ENV MODE=${MODE}
 ENV COMPILER=${COMPILER}
@@ -16,20 +19,18 @@ ENV COMPILER_VERSION=${COMPILER_VERSION}
 ENV COMPILER_LIBCXX=${COMPILER_LIBCXX}
 ENV CC=${CC}
 ENV CXX=${CXX}
-ENV PACKAGE=${PACKAGE}
 ENV CONAN_TOKEN=${CONAN_TOKEN}
+ENV NPP_CI=${NPP_CI}
 
-WORKDIR /code
-ADD requirements.txt /code/
+RUN mkdir -p /tmp
+WORKDIR /tmp
+COPY requirements.txt /tmp/
 RUN pip install -r requirements.txt
-
-RUN curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-RUN sudo apt-get install -y nodejs
 
 RUN conan remote add bincrafters "https://api.bintray.com/conan/bincrafters/public-conan"
 RUN conan remote add npm-mas-mas "https://api.bintray.com/conan/npm-mas-mas/testing"
 RUN conan user -p $CONAN_TOKEN -r npm-mas-mas makiolo
 
-COPY . /code
-RUN bash -c /code/entrypoint.sh
+COPY --chown=1000:1001 . /tmp
+cmd /tmp/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,16 @@
-#!//bin/bash
+#!/bin/bash
+
+set -e
 
 export MODE="${MODE:-Debug}"
-export COMPILER="${COMPILER:-gcc}"
-export COMPILER_LIBCXX="${COMPILER_LIBCXX:-libstdc++11}"
-export COMPILER_VERSION="${COMPILER_VERSION:-7.3}"
+export COMPILER="${COMPILER:-$(conan profile show default | grep -e "\<compiler\>=" | cut -d"=" -f2)}"
+export COMPILER_LIBCXX="${COMPILER_LIBCXX:-$(conan profile show default | grep -e "\<compiler.libcxx\>=" | cut -d"=" -f2)}"
+export COMPILER_VERSION="${COMPILER_VERSION:-$(conan profile show default | grep -e "\<compiler.version\>=" | cut -d"=" -f2)}"
 
-if [ "$(uname)" == "Darwin" ]; then
-	# mac
-	export COMPILER=apple-clang COMPILER_VERSION=10.0 COMPILER_LIBCXX=libc++
+if [ "$NPP_CI" == "FALSE" ]; then
+	conan install . --build missing -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION
 fi
 
-# compile 3rd parties
-# conan install . --build missing -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION
-# conan create . npm-mas-mas/testing --build $PACKAGE -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION -tf None
-# conan upload $PACKAGE/*@npm-mas-mas/testing -r npm-mas-mas --all -c
-
-# compile only $PACKAGE
 conan create . npm-mas-mas/testing -s compiler=$COMPILER -s build_type=$MODE -s compiler.libcxx=$COMPILER_LIBCXX -s compiler.version=$COMPILER_VERSION -tf None
-conan upload *@npm-mas-mas/testing -r npm-mas-mas --all -c
+conan upload '*' -r npm-mas-mas --all -c
 
